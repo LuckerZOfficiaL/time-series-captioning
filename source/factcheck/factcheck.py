b345bef21f675493b1e93b2224e7d6fa3d4f6efb
Luca Zhou <lucazhou2000@gmail.com>
2025-03-21 03:51:53 +0000

    add intermediate savings in factcheck.py

--- source/factcheck/factcheck.py
+++ source/factcheck/factcheck.py
@@ -74,6 +74,7 @@ def main():
             start_idx = 0
 
         for i in range(start_idx, len(fake_facts)):
+            print(f"Refing fake fact {i+1}/{len(fake_facts)}")
             revised_fact = refine_caption_with_corrected_facts(fake_facts[i],
                                                                 model=config['model']['refinement_model'],
                                                                 correction_method="llm",
@@ -82,8 +83,7 @@ def main():
                                                                 extract_sentences=True,
                                                                 )
             llm_revised_fake_facts.append(revised_fact)
-            print(f"Refined fake fact {i+1}/{len(fake_facts)}")
-
+            
             if config['factcheck']['save_files']:
                 save_file(str(i+1), '/home/ubuntu/thesis/source/factcheck/fake_count.txt', 'w')
                 save_file(llm_revised_fake_facts[-1], f'/home/ubuntu/thesis/source/factcheck/{config['model']['refinement_model']}_llm_revised_fake_facts.txt', 'a')
@@ -97,6 +97,7 @@ def main():
         else:
             start_idx = 0
         for i in range(start_idx, len(true_facts)):
+            print(f"Refining true fact {i+1}/{len(true_facts)}")
             revised_fact = refine_caption_with_corrected_facts(true_facts[i],
                                                                 model=config['model']['refinement_model'],
                                                                 correction_method="llm",
@@ -104,8 +105,7 @@ def main():
                                                                 skip_numeric=False,
                                                                 extract_sentences=True)
             llm_revised_true_facts.append(revised_fact)
-            print(f"Refined true fact {i+1}/{len(true_facts)}")
-
+            
             if config['factcheck']['save_files']:
                 save_file(str(i+1), '/home/ubuntu/thesis/source/factcheck/true_count.txt', 'w')
                 save_file(llm_revised_true_facts[-1], f'/home/ubuntu/thesis/source/factcheck/{config['model']['refinement_model']}_llm_revised_true_facts.txt', 'a')
@@ -144,81 +144,111 @@ def main():
 
 
     ############### Metric EVALUATION #####################
-    inclusions = 0
-    conflicts = 0
-    equivalences = 0
-    wrong_gts = 0
-    i = 1
+    if os.path.exists('/home/ubuntu/thesis/source/factcheck/fake_evals.json'):
+            with open('/home/ubuntu/thesis/source/factcheck/fake_evals.json', 'r') as file:
+                fake_evals = json.load(file)
+    else:
+        fake_evals = {
+            "inclusions": 0,
+            "conflicts": 0,
+            "equivalences": 0,
+            "wrong_gts": 0,
+            "count": 0
+
+        }
+
+    inclusions = fake_evals['inclusions']
+    conflicts = fake_evals['conflicts']
+    equivalences = fake_evals['equivalences']
+    wrong_gts = fake_evals['wrong_gts']
+    start_idx = fake_evals['count']
+
     print("\n\nEvaluating Fake Facts...")
-    for original_fake_fact, llm_revised_fake_fact, gt_revised_fake_fact in zip(fake_facts, llm_revised_fake_facts, gt_revised_fake_facts):
-        print(f"\nEvaluating {i}/{len(fake_facts)})")
-        i += 1
-        if are_semantically_equivalent(llm_revised_fake_fact, gt_revised_fake_fact, model=config['model']['checking_model']):
-            equivalences += 1
+    for i in range(start_idx, len(fake_facts)):
+        print(f"\nEvaluating {i+1}/{len(fake_facts)})")
+        if are_semantically_equivalent(llm_revised_fake_facts[i], gt_revised_fake_facts[i], model=config['model']['checking_model']):
+            fake_evals['equivalences'] += 1
             print("\nSEMANTICALLY EQUIVALENT!")
-        elif is_semantically_contained(gt_revised_fake_fact, llm_revised_fake_fact, model=config['model']['checking_model']):
-            inclusions += 1
+        elif is_semantically_contained(gt_revised_fake_facts[i], llm_revised_fake_facts[i], model=config['model']['checking_model']):
+            fake_evals['inclusions'] += 1
             print("\nSEMANTICALLY INCLUDED!")
-        elif are_semantically_conflicting(llm_revised_fake_fact, gt_revised_fake_fact, model=config['model']['checking_model']):
-            conflicts += 1
+        elif are_semantically_conflicting(llm_revised_fake_facts[i], gt_revised_fake_facts[i], model=config['model']['checking_model']):
+            fake_evals['conflicts'] += 1
             print("\nCONFLICT!")
-            comparison = compare_correctness(llm_revised_fake_fact, gt_revised_fake_fact, model=config['model']['checking_model'])
+            comparison = compare_correctness(llm_revised_fake_facts[i], gt_revised_fake_facts[i], model=config['model']['checking_model'])
             if comparison == 1:
                 print("But actually GT is wrong!")
-                wrong_gts += 1
+                fake_evals['wrong_gts'] += 1
             elif comparison == 2:
                 print("Your method got it wrong! :(")
             else:
                 print("Failed to compare the two head-to-head.")
+            fake_evals['count'] += 1
 
         else: print("\nNOTHING DETECTED!")
-        print(f"Original Fake Fact: {original_fake_fact} \nLLM: {llm_revised_fake_fact} \nGT: {gt_revised_fake_fact}")
+        print(f"Original Fake Fact: {original_fake_facts[i]} \nLLM: {llm_revised_fake_facts[i]} \nGT: {gt_revised_fake_facts[i]}")
+        save_file(fake_evals, '/home/ubuntu/thesis/source/factcheck/fake_evals.json')
         
     print("\nFake Facts:")
-    print(f"Equivalences: {equivalences}/{len(fake_facts)}")
-    print(f"Inclusions: {inclusions}/{len(fake_facts)}")
-    print(f"Conflicts: {conflicts}/{len(fake_facts)}")
-    print(f"Inconclusive: {len(fake_facts)-equivalences-inclusions-conflicts}/{len(fake_facts)}")
-    print(f"Wrong ground truth refinements: {wrong_gts}/{len(true_facts)}")
+    print(f"Equivalences: {fake_evals['equivalences']}/{len(fake_facts)}")
+    print(f"Inclusions: {fake_evals['inclusions']}/{len(fake_facts)}")
+    print(f"Conflicts: {fake_evals['conflicts']}/{len(fake_facts)}")
+    print(f"Inconclusive: {len(fake_facts)-fake_evals['equivalences']-fake_evals['inclusions']-fake_evals['conflicts']}/{len(fake_facts)}")
+    print(f"Wrong ground truth refinements: {fake_evals['wrong_gts']}/{len(true_facts)}")
+    save_file(fake_evals, '/home/ubuntu/thesis/source/factcheck/fake_evals.json')
 
 
+    if os.path.exists('/home/ubuntu/thesis/source/factcheck/true_evals.json'):
+            with open('/home/ubuntu/thesis/source/factcheck/true_evals.json', 'r') as file:
+                true_evals = json.load(file)
+    else:
+        true_evals = {
+            "inclusions": 0,
+            "conflicts": 0,
+            "equivalences": 0,
+            "wrong_gts": 0,
+            "count": 0
 
-    inclusions = 0
-    conflicts = 0
-    equivalences = 0
-    wrong_gts = 0
-    i = 1
+        }
+
+    inclusions = true_evals['inclusions']
+    conflicts = true_evals['conflicts']
+    equivalences = true_evals['equivalences']
+    wrong_gts = true_evals['wrong_gts']
+    start_idx = true_evals['count']
+    
     print("\n\nEvaluating True Facts...")
-    for original_true_fact, llm_revised_true_fact, gt_revised_true_fact in zip(true_facts, llm_revised_true_facts, gt_revised_true_facts):
-        print(f"\nEvaluating {i}/{len(true_facts)})")
-        i += 1
-        if are_semantically_equivalent(llm_revised_true_fact, gt_revised_true_fact):
-            equivalences += 1
+    for i in range(start_idx, len(true_facts)):
+        print(f"\nEvaluating {i+1}/{len(true_facts)})")
+        if are_semantically_equivalent(llm_revised_true_facts[i], gt_revised_true_facts[i]):
+            true_evals['equivalences'] += 1
             print("\nSEMANTICALLY EQUIVALENT!")
-        elif is_semantically_contained(gt_revised_true_fact, llm_revised_true_fact):
-            inclusions += 1
+        elif is_semantically_contained(gt_revised_true_facts[i], llm_revised_true_facts[i]):
+            true_evals['inclusios'] += 1
             print("\nSEMANTICALLY INCLUDED!")
-        elif are_semantically_conflicting(llm_revised_true_fact, gt_revised_true_fact):
-            conflicts += 1
+        elif are_semantically_conflicting(llm_revised_true_facts[i], gt_revised_true_facts[i]):
+            true_evals['conflicts'] += 1
             print("\nCONFLICT!")
-            comparison = compare_correctness(llm_revised_true_fact, gt_revised_true_fact, model=config['model']['checking_model'])
+            comparison = compare_correctness(llm_revised_true_facts[i], gt_revised_true_facts[i], model=config['model']['checking_model'])
             if comparison == 1:    
                 print("But actually GT is wrong!")
-                wrong_gts += 1
+                true_evals['wrong_gts'] += 1
             elif comparison == 2:
                 print("Your method got it wrong! :(")
             else:
                 print("Failed to compare the two head-to-head.")
         else: print("\nNOTHING DETECTED!")
-        print(f"Original True Fact: {original_true_fact} \nLLM: {llm_revised_true_fact} \nGT: {gt_revised_true_fact}")
+        print(f"Original True Fact: {original_true_facts[i]} \nLLM: {llm_revised_true_facts[i]} \nGT: {gt_revised_true_facts[i]}")
+        save_file(fake_evals, '/home/ubuntu/thesis/source/factcheck/true_evals.json')
         
 
     print("\nTrue Facts:")
-    print(f"Equivalences: {equivalences}/{len(true_facts)}")
-    print(f"Inclusions: {inclusions}/{len(true_facts)}")
-    print(f"Conflicts: {conflicts}/{len(true_facts)}")
-    print(f"Inconclusive: {len(true_facts)-equivalences-inclusions-conflicts}/{len(true_facts)}")
-    print(f"Wrong ground truth refinements: {wrong_gts}/{len(true_facts)}")
+    print(f"Equivalences: {true_evals['equivalences']}/{len(true_facts)}")
+    print(f"Inclusions: {true_evals['inclusions']}/{len(true_facts)}")
+    print(f"Conflicts: {true_evals['conflicts']}/{len(true_facts)}")
+    print(f"Inconclusive: {len(true_facts)-true_evals['equivalences']-true_evals['inclusions']-true_evals['conflicts']}/{len(true_facts)}")
+    print(f"Wrong ground truth refinements: {true_evals['wrong_gts']}/{len(true_facts)}")
+    save_file(true_evals, '/home/ubuntu/thesis/source/factcheck/true_evals.json')
 
 if __name__ == "__main__":
     #main()
