7599dae63fcd2e305c4a4794b916bd5c9676ce7a
Marshall Fisher <m1fisher@ucsd.edu>
2025-04-11 18:48:19 +0000

    get batch inference working

--- source/infer_model_captions.py
+++ source/infer_model_captions.py
@@ -3,6 +3,7 @@ import json
 import os
 from pathlib import Path
 import re
+import time
 
 ### Re-use imports from llava/eval/run_llava.py ###
 import argparse
@@ -37,9 +38,49 @@ from llava.eval.run_llava import image_parser, load_images
 from helpers import generate_prompt_for_baseline
 
 #MODEL_PATH = "liuhaotian/llava-v1.6-34b"
-MODEL_PATH = "liuhaotian/llava-v1.6-mistral-7b"
+MODEL_PATH = "llava-hf/llava-v1.6-mistral-7b-hf"
 DATA_DIR = "/home/ubuntu/time-series-captioning/data/samples/"
 OUT_DIR = "/home/ubuntu/time-series-captioning/llava_captions_mistral7b"
+
+
+import requests
+from PIL import Image
+import torch
+from transformers import AutoProcessor, AutoModelForImageTextToText, BitsAndBytesConfig
+
+
+@lru_cache
+def _load_batch_llava_model(model_name):
+    model = AutoModelForImageTextToText.from_pretrained(
+        model_name,
+        torch_dtype=torch.float16,
+        low_cpu_mem_usage=True,
+    ).to(0)
+    processor = AutoProcessor.from_pretrained(model_name, use_fast=True)
+    return model, processor
+
+def eval_batch_llava(prompts, image_files):
+    prompts = ["What is displayed in this image? Please answer in detail in 4 sentences." for x in range(10)]
+    conversations = [{
+            "role": "user",
+            "content": [
+                {"type": "image"},
+                {"type": "text", "text": f"{prompt}"},
+            ],
+    } for prompt in prompts]
+    images = [Image.open(fn) for fn in image_files]
+    model, processor = _load_batch_llava_model(MODEL_PATH)
+    prompts = [processor.apply_chat_template([c], add_generation_prompt=True)
+               for c in conversations]
+    inputs = processor(images=images, text=prompts, padding=True, return_tensors="pt").to(model.device)
+    stime = time.time()
+    generate_ids = model.generate(**inputs, max_new_tokens=512, temperature=0.3, do_sample=True)
+    print("BATCH TIME:", time.time() - stime)
+    results = processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+    # Remove original prompt from returned result
+    captions = [r.split('[/INST] ')[1] for r in results]
+    return captions
+
 
 def _eval_model(prompt, image_file):
     """
@@ -49,9 +90,9 @@ def _eval_model(prompt, image_file):
         "model_path": MODEL_PATH,
         "model_base": None,
         "model_name": get_model_name_from_path(MODEL_PATH),
-        "query": prompt,
+        "queries": prompt,
         "conv_mode": None,
-        "image_file": image_file,
+        "image_files": image_file,
         "sep": ",",
         "temperature": 0.3,  # NOTE: We set temperature to 0.3
         "top_p": None,
@@ -76,7 +117,7 @@ def eval_llava(args):
 
     model_name = get_model_name_from_path(args.model_path)
     tokenizer, model, image_processor, context_len = _load_llava_model(args.model_path, args.model_base, model_name)
-
+    import pdb; pdb.set_trace()
     qs = args.query
     image_token_se = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN
     if IMAGE_PLACEHOLDER in qs:
@@ -149,29 +190,37 @@ def eval_llava(args):
     print(outputs)
     return outputs
 
-def write_caption(ts_name):
+def write_caption(ts_names):
     """
     Given ts_name, write a caption .txt file for the time series.
     """
-    dataset_name = ts_name.split("_")[0]
-    with open(os.path.join(DATA_DIR, "metadata", f"{ts_name}.json"), 'r') as fh:
-        metadata = json.load(fh)
-    with open(os.path.join(DATA_DIR, "time series", f"{ts_name}.txt"), 'r') as fh:
-        ts = fh.read()
-    prompt = generate_prompt_for_baseline(dataset_name, metadata, ts)
-    image_file = os.path.join(DATA_DIR, "plots_2.0", f"{ts_name}.jpeg")
-    caption = _eval_model(prompt, image_file)
-    with open(os.path.join(OUT_DIR, f"{ts_name}.txt"), "w+") as fh:
-        fh.write(caption)
+    prompts = []
+    image_files = []
+    for ts_name in ts_names:
+      dataset_name = ts_name.split("_")[0]
+      with open(os.path.join(DATA_DIR, "metadata", f"{ts_name}.json"), 'r') as fh:
+          metadata = json.load(fh)
+      with open(os.path.join(DATA_DIR, "time series", f"{ts_name}.txt"), 'r') as fh:
+          ts = fh.read()
+      prompt = generate_prompt_for_baseline(dataset_name, metadata, ts)
+      image_file = os.path.join(DATA_DIR, "plots_2.0", f"{ts_name}.jpeg")
+      prompts.append(prompt)
+      image_files.append(image_file)
+
+    captions = eval_batch_llava(prompts, image_files)
+    for ts_name, caption in zip(ts_names, captions):
+        with open(os.path.join(OUT_DIR, f"{ts_name}.txt"), "w+") as fh:
+            fh.write(caption)
 
 
 def main():
     ts_names = [Path(fn).stem for fn in os.listdir(os.path.join(DATA_DIR, "time series"))]
     done_names = {Path(fn).stem for fn in os.listdir(OUT_DIR)}
     ts_names = sorted([name for name in ts_names if name not in done_names])
-    for ts_name in ts_names:
-        print(f"Writing caption for {ts_name}")
-        write_caption(ts_name)
+    batch_size = 20
+    for i in range(0, len(ts_names), batch_size):
+        ts_batch = ts_names[i:i+batch_size]
+        write_caption(ts_batch)
 
 if __name__ == "__main__":
     main()
