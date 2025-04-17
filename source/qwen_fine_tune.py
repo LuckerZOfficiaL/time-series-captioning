c157fa2ab5746102c7ee0ce1c89d98361f0a4953
Marshall Fisher <m1fisher@ucsd.edu>
2025-04-17 20:14:10 +0000

    new fine tuning code

--- source/qwen_fine_tune.py
+++ source/qwen_fine_tune.py
@@ -5,13 +5,14 @@ from pathlib import Path
 import re
 import time
 
+from datasets import Dataset
 import requests
 from PIL import Image
 from qwen_omni_utils import process_mm_info
 import torch
 from transformers import Qwen2_5OmniForConditionalGeneration, Qwen2_5OmniProcessor
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from trl import SFTConfig
+from trl import SFTConfig, SFTTrainer
 
 from helpers import generate_prompt_for_baseline
 
@@ -55,7 +56,6 @@ def get_train_dataset(data_dir, processor):
     prompts = []
     image_files = []
     labels = []
-    ts_names = ts_names[:100]
     print(f"Loading time series dataset, length {len(ts_names)}")
     for ts_name in ts_names:
         dataset_name = ts_name.split("_")[0]
@@ -72,8 +72,9 @@ def get_train_dataset(data_dir, processor):
         image_file = os.path.join(data_dir, "plots", f"{ts_name}.jpeg")
         prompts.append(prompt)
         image_files.append(image_file)
-    # TODO: gather labels 
-    return [format_conversation(p, i, l, processor) for p, i, l in zip(prompts, image_files, labels)]
+    conversations = [format_conversation(p, i, l, processor) for p, i, l in zip(prompts, image_files, labels)]
+    conversations = [processor.apply_chat_template(c, add_generation_prompt=True, tokenize=False)[0] for c in conversations] 
+    return [{"chat": c, "image": i} for c, i in zip(conversations, image_files)]
 
 def eval_batch_qwen(prompts, image_files, device, use_image): 
     model, processor = _load_batch_qwen_model(MODEL_PATH, device)
@@ -91,29 +92,22 @@ def eval_batch_qwen(prompts, image_files, device, use_image):
     return captions
 
 def collate_fn(examples):
-    # Get the texts and images, and apply the chat template
-    texts = [
-        processor.apply_chat_template(example, add_generation_prompt=True, tokenize=False) for example in examples
-    ]  # Prepare texts for processing
-    _, images, _ = process_mm_info(examples, use_audio_in_video=False)
+    _, processor = _load_batch_qwen_model(MODEL_PATH, 'cuda')
+    text = [ex["chat"] for ex in examples]
+    #text = [processor.apply_chat_template(c, add_generation_prompt=True, tokenize=False)[0] for c in examples] 
+#    _, images, _ = process_mm_info(examples)
+    images = [Image.open(ex["image"]) for ex in examples]
     # Tokenize the texts and process the images
     batch = processor(
-        text=texts, images=images, return_tensors="pt", padding=True, use_audio_in_video=False
-    )  # Encode texts and images into tensors
-
+        text=text, images=images, return_tensors="pt", padding=True
+    ) 
     # The labels are the input_ids, and we mask the padding tokens in the loss computation
     labels = batch["input_ids"].clone()  # Clone input IDs for labels
     labels[labels == processor.tokenizer.pad_token_id] = -100  # Mask padding tokens in labels
-    
-    import pdb; pdb.set_trace()
-
-    # Ignore the image token index in the loss computation (model specific)
-    if isinstance(processor, Qwen2VLProcessor):  # Check if the processor is Qwen2VLProcessor
-        image_tokens = [151652, 151653, 151655]  # Specific image token IDs for Qwen2VLProcessor
-    else:
-        image_tokens = [processor.tokenizer.convert_tokens_to_ids(processor.image_token)]  # Convert image token to ID
-
+    # TODO: Make sure this image token masking is correct
+    # Ignore the image token index in the loss computation 
     # Mask image token IDs in the labels
+    image_tokens = [processor.tokenizer.convert_tokens_to_ids(processor.image_token)]  # Convert image token to ID
     for image_token_id in image_tokens:
         labels[labels == image_token_id] = -100  # Mask image token IDs in labels
 
@@ -125,14 +119,29 @@ def main(model_eval, data_dir, out_dir, use_image=True):
     device = 'cuda'
     model, processor = _load_batch_qwen_model(MODEL_PATH, device)
     training_data = get_train_dataset(data_dir, processor)
+    train_dataset = Dataset.from_list(training_data)
+
     training_args = SFTConfig(
         output_dir=out_dir,
         num_train_epochs=1,
         learning_rate=1e-4,
-        optim="adamw_torch_fused"
+        optim="adamw_torch_fused",
+        remove_unused_columns=False,
     )
-    collate_fn(training_data)
-    import pdb; pdb.set_trace()
+    training_args.dataset_kwargs = {"skip_prepare_dataset": True}
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+#        eval_dataset=eval_dataset,
+        data_collator=collate_fn,
+#        peft_config=peft_config,
+#        tokenizer=processor.tokenizer,
+    )
+    print("Now training model")
+    trainer.train()
+    print("Model training complete, now saving")
+    trainer.save_model(training_args.output_dir)
 
     
 if __name__ == "__main__":
