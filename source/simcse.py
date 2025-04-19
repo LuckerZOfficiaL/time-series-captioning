be5df75f43e732c0641f55ab0cc5d4201d190c23
Luca Zhou <lucazhou2000@gmail.com>
2025-04-19 03:01:29 +0000

    add simcse for loop

--- source/simcse.py
+++ source/simcse.py
@@ -7,69 +7,74 @@ import re
 from collections import defaultdict
 import json
 
-name = "internvl-finetune11"
 
-gt_dir = "/home/ubuntu/thesis/data/samples/new samples no overlap/test/gt_captions"
-gen_dir = "/home/ubuntu/thesis/data/samples/new samples no overlap/generated captions/internvl-finetune-pratham"
 
-model_path = "/home/ubuntu/thesis/data/simcse_ts"
+model_path = "/home/ubuntu/thesis/data/simcse_ts_new"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModel.from_pretrained(model_path).eval().to(device)
-
-# def is_multiple_of_3(fname):
-#     match = re.search(r"_(\d+)\.txt$", fname)
-#     return match and int(match.group(1)) % 3 == 0
 
 def get_domain(fname):
     return fname.split("_")[0]
 
 domain_similarities = defaultdict(list)
 
-gt_all = {f for f in os.listdir(gt_dir) if f.endswith(".txt")}
-gen_all = set(os.listdir(gen_dir))
-common_files = sorted(gt_all & gen_all)
 
-for fname in tqdm(common_files):
-    gt_path = os.path.join(gt_dir, fname)
-    gen_path = os.path.join(gen_dir, fname)
+generated_folders = [d for d in os.listdir("/home/ubuntu/thesis/data/samples/new samples no overlap/generated captions") if os.path.isdir(os.path.join("/home/ubuntu/thesis/data/samples/new samples no overlap/generated captions", d))]
 
-    if not os.path.exists(gen_path):
-        continue
+for name in generated_folders:
+#try:
+    name = "positive pair example"
+    gt_dir = f"/home/ubuntu/thesis/data/samples/simcse experiment/simcse negative experiment gt"
+    gen_dir = f"/home/ubuntu/thesis/data/samples/simcse experiment/simcse negative experiment gen"
 
-    with open(gt_path, "r") as f:
-        gt_text = f.read().strip()
-    with open(gen_path, "r") as f:
-        gen_text = f.read().strip()
+    gt_all = {f for f in os.listdir(gt_dir) if f.endswith(".txt")}
+    gen_all = set(os.listdir(gen_dir))
+    common_files = sorted(gt_all & gen_all)
 
-    texts = [gt_text, gen_text]
-    inputs = tokenizer(texts, padding=True, truncation=True, return_tensors="pt").to(device)
+    for fname in tqdm(common_files):
+        gt_path = os.path.join(gt_dir, fname)
+        gen_path = os.path.join(gen_dir, fname)
 
-    with torch.no_grad():
-        embeddings = model(**inputs).pooler_output
-        embeddings = F.normalize(embeddings, p=2, dim=1)
-        similarity = F.cosine_similarity(embeddings[0].unsqueeze(0), embeddings[1].unsqueeze(0)).item()
+        if not os.path.exists(gen_path):
+            continue
 
-    domain = get_domain(fname)
-    domain_similarities[domain].append(similarity)
+        with open(gt_path, "r") as f:
+            gt_text = f.read().strip()
+        with open(gen_path, "r") as f:
+            gen_text = f.read().strip()
+
+        texts = [gt_text, gen_text]
+        inputs = tokenizer(texts, padding=True, truncation=True, return_tensors="pt").to(device)
+
+        with torch.no_grad():
+            embeddings = model(**inputs).pooler_output
+            embeddings = F.normalize(embeddings, p=2, dim=1)
+            similarity = F.cosine_similarity(embeddings[0].unsqueeze(0), embeddings[1].unsqueeze(0)).item()
+
+        domain = get_domain(fname)
+        domain_similarities[domain].append(similarity)
 
 
-domain_avg_json = {}
-print("\average SimCSE by domain:")
-for domain, sims in sorted(domain_similarities.items()):
-    avg = sum(sims) / len(sims)
-    domain_avg_json[domain] = {
-        "avg_sim": round(avg, 4)
+    domain_avg_json = {}
+    print(f"\n{name}\nAverage SimCSE by domain:")
+    for domain, sims in sorted(domain_similarities.items()):
+        avg = sum(sims) / len(sims)
+        domain_avg_json[domain] = {
+            "avg_sim": round(avg, 4)
+        }
+        print(f"{domain:<20}: {avg:.4f}  (n={len(sims)})")
+
+    all_sims = [sim for sims in domain_similarities.values() for sim in sims]
+    global_avg = sum(all_sims) / len(all_sims)
+    domain_avg_json["global_avg"] = {
+        "avg_sim": round(global_avg, 4)
     }
-    print(f"{domain:<20}: {avg:.4f}  (n={len(sims)})")
+    print(f"global average: {global_avg:.4f}")
 
-all_sims = [sim for sims in domain_similarities.values() for sim in sims]
-global_avg = sum(all_sims) / len(all_sims)
-domain_avg_json["global_avg"] = {
-    "avg_sim": round(global_avg, 4)
-}
-print(f"\nglobal average: {global_avg:.4f}")
-
-with open(f"/home/ubuntu/thesis/data/evaluation results/simcse_results/{name}.json", "w") as f:
-    json.dump(domain_avg_json, f, indent=2)
+    #with open(f"/home/ubuntu/thesis/data/evaluation results/simcse_results/{name}.json", "w") as f:
+    #    json.dump(domain_avg_json, f, indent=2)
+        
+#except Exception as e:
+#    print(e)
